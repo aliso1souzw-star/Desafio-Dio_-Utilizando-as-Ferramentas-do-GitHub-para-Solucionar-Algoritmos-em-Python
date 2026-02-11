90e986d58a2f58da5213f3475d0fce37cab6174f
alyssonsouza <aliso.1souzw@gmail.com>
2026-02-11 16:08:56 +0000

    Atualizando

---
+++ Desafio-Dio/concat_dados.py
@@ -0,0 +1,8 @@
+# Vamos receber dois dados diferentes do usuario e concatenar eles, ou seja, juntar os dois dados em um unico dado
+
+info1 = input("Digite o primeiro dado: ")
+info2 = input("Digite o segundo dado: ")
+
+info_concatenada = info1 + " " + info2
+
+print("O dado concatenado é: " + info_concatenada)
