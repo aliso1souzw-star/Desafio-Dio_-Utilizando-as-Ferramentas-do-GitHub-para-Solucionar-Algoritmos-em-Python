e36bba932988d6b0c86567dfb40d57c1755c6ae1
alyssonsouza <aliso.1souzw@gmail.com>
2026-02-11 16:33:11 +0000

    atualizando

---
+++ Desafio-Dio/repet_txt.py
@@ -0,0 +1,7 @@
+# Agora vamos solicitar uma string e um numero inteiro como entrada, Depois teremos que retornar a string repetida o numero de vezes do informado.
+
+texto = input("Digite uma string: ")
+numero = int(input("Digite um numero inteiro: "))
+
+texto_repetido = texto * numero
+print("A string repetida é:  " + texto_repetido)
